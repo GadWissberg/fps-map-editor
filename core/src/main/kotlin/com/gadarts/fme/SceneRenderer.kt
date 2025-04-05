1f52410b90f5737d3360de73adec4c9b07fdf4cb
gad.wissberg <gad.wissberg@riskified.com>
2025-04-05 22:46:47 +0300

    Adding creation.

--- core/src/main/kotlin/com/gadarts/fme/SceneRenderer.kt
+++ core/src/main/kotlin/com/gadarts/fme/SceneRenderer.kt
@@ -3,22 +3,22 @@ package com.gadarts.fme
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.InputProcessor
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.PerspectiveCamera
-import com.badlogic.gdx.graphics.g3d.Material
-import com.badlogic.gdx.graphics.g3d.Model
-import com.badlogic.gdx.graphics.g3d.ModelBatch
-import com.badlogic.gdx.graphics.g3d.ModelInstance
+import com.badlogic.gdx.graphics.VertexAttributes
+import com.badlogic.gdx.graphics.g3d.*
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
-import com.badlogic.gdx.math.Intersector
-import com.badlogic.gdx.math.Plane
-import com.badlogic.gdx.math.Vector2
-import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.math.*
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.Disposable
+import kotlin.math.abs
 
 class SceneRenderer : Table(), InputProcessor, Disposable {
+    private val initialDrawingPoint = Vector3()
+    private var drawingBlock: ModelInstance? = null
     private val blocks = mutableListOf<ModelInstance>()
     private var mode: Modes? = Modes.CREATE
     private val auxiliaryModelInstances = AuxiliaryModelInstances()
@@ -26,10 +26,28 @@ class SceneRenderer : Table(), InputProcessor, Disposable {
     private val camera: PerspectiveCamera = createCamera()
     private val cameraController = CameraInputController(camera)
     private val cubeModel = createCubeModel()
+    private val environment = createEnvironment()
+
+    private fun createEnvironment(): Environment {
+        val environment = Environment()
+        environment.set(ColorAttribute.createAmbientLight(0.5f, 0.5f, 0.5f, 1f))
+        val directionalLight = DirectionalLight()
+        directionalLight.set(Color.WHITE, 0.5f, -0.8f, -0.2f)
+        environment.add(directionalLight)
+        return environment
+    }
 
     private fun createCubeModel(): Model {
         val modelBuilder = ModelBuilder()
-        return modelBuilder.createBox(0.5F, 0.5F, 0.5F, Material(), ColorAttribute.Diffuse)
+
+        return modelBuilder.createBox(
+            1f, 1f, 1f, // width, height, depth
+            Material(ColorAttribute.createDiffuse(Color.GRAY)),
+            (VertexAttributes.Usage.Position or VertexAttributes.Usage.Normal).toLong()
+        ).apply {
+            nodes.first().translation.set(0.5f, 0.5f, 0.5f)
+            calculateTransforms()
+        }
     }
 
     private fun createCamera(): PerspectiveCamera {
@@ -59,7 +77,10 @@ class SceneRenderer : Table(), InputProcessor, Disposable {
         modelsBatch.begin(camera)
         auxiliaryModelInstances.render(modelsBatch)
         blocks.forEach {
-            modelsBatch.render(it)
+            modelsBatch.render(it, environment)
+        }
+        if (drawingBlock != null) {
+            modelsBatch.render(drawingBlock, environment)
         }
         modelsBatch.end()
     }
@@ -80,6 +101,8 @@ class SceneRenderer : Table(), InputProcessor, Disposable {
 
     companion object {
         private val auxVector2 = Vector2()
+        private val auxMatrix = Matrix4()
+        private const val WORLD_SIZE = 40F
     }
 
     override fun keyDown(keycode: Int): Boolean {
@@ -106,11 +129,13 @@ class SceneRenderer : Table(), InputProcessor, Disposable {
             val hit = getFirstPlaneIntersection(screenX, screenY, camera, scenePlanes)
             if (hit != null) {
                 val (_, point) = hit
+                point.set(point.x.toInt().toFloat(), point.y.toInt().toFloat(), point.z.toInt().toFloat())
                 val modelInstance = ModelInstance(cubeModel)
                 modelInstance.transform.setTranslation(point)
-                blocks.add(modelInstance)
+                drawingBlock = modelInstance
+                initialDrawingPoint.set(point)
+                auxMatrix.set(drawingBlock!!.transform)
             }
-
             return true
         }
         return false
@@ -146,6 +171,11 @@ class SceneRenderer : Table(), InputProcessor, Disposable {
     }
 
     override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        if (drawingBlock != null) {
+            blocks.add(drawingBlock!!)
+            drawingBlock = null
+            return true
+        }
         return false
     }
 
@@ -154,15 +184,33 @@ class SceneRenderer : Table(), InputProcessor, Disposable {
     }
 
     private val scenePlanes = listOf(
-        ScenePlane(Plane(Vector3.Y, 0f), "floor"),                // y = 0
-        ScenePlane(Plane(Vector3.Y.cpy().scl(-1f), 10f), "ceiling"),  // y = 10
-        ScenePlane(Plane(Vector3.X, 0F), "leftWall"),           // x = -5
-        ScenePlane(Plane(Vector3.X.cpy().scl(-1f), 5f), "rightWall"), // x = 5
-        ScenePlane(Plane(Vector3.Z, 0F), "backWall"),           // z = -5
-        ScenePlane(Plane(Vector3.Z.cpy().scl(-1f), 5f), "frontWall")  // z = 5
+        ScenePlane(Plane(Vector3.Y, 0f), "floor"),
+        ScenePlane(Plane(Vector3.Y.cpy().scl(-1f), WORLD_SIZE), "ceiling"),
+        ScenePlane(Plane(Vector3.X, 0F), "leftWall"),
+        ScenePlane(Plane(Vector3.X.cpy().scl(-1f), WORLD_SIZE), "rightWall"),
+        ScenePlane(Plane(Vector3.Z, 0F), "backWall"),
+        ScenePlane(Plane(Vector3.Z.cpy().scl(-1f), WORLD_SIZE), "frontWall")
     )
 
     override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        if (drawingBlock != null) {
+            val relativeX = screenX.toFloat()
+            val relativeY = screenY.toFloat()
+
+            camera.getPickRay(relativeX, relativeY)
+            val hit = getFirstPlaneIntersection(screenX, screenY, camera, scenePlanes)
+            if (hit != null) {
+                val (_, point) = hit
+                point.set(point.x.toInt().toFloat(), point.y.toInt().toFloat(), point.z.toInt().toFloat())
+                drawingBlock!!.transform.set(auxMatrix)
+                drawingBlock!!.transform.scl(
+                    if (abs(point.x - initialDrawingPoint.x) > 1F) point.x - initialDrawingPoint.x else 1F,
+                    1F,
+                    if (abs(point.z - initialDrawingPoint.z) > 1F) point.z - initialDrawingPoint.z else 1F
+                )
+                return true
+            }
+        }
         return false
     }
 
@@ -178,4 +226,5 @@ class SceneRenderer : Table(), InputProcessor, Disposable {
         cubeModel.dispose()
         auxiliaryModelInstances.dispose()
     }
+
 }
